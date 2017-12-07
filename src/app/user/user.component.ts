b48665a01b5c2e8524aeb8f774d8f2b43410b129
Mohamed <idbrahimdev@gmail.com>
2017-12-07 17:21:02 +0000

    show user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
+import { UsersService } from './../users.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  
+  idUser;
+  posts: any[];
+  constructor(private route: ActivatedRoute, private userService: UsersService) { }
 
   ngOnInit() {
+    
+    this.route.paramMap.subscribe(response => {
+      this.idUser = response.get('id');
+    }) 
+    this.userService.getPostsUser(this.idUser)
+    .subscribe(response => {
+      console.log(response.json())
+      this.posts = response.json();
+    })
   }
+
+ 
 
 }
